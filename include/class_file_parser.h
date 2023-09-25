8351a477f1b7dff414b32de339e456217f348028
Ledmington <ledmington.dev@gmail.com>
2023-09-25 23:50:18 +0200

    Minor fixes

--- include/class_file_parser.h
+++ include/class_file_parser.h
@@ -3,6 +3,8 @@
 
 #include <stdarg.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>	 // memcpy
 
 // Useful typedefs to be closer to the official specification
@@ -21,28 +23,24 @@ typedef int64_t i8;
 #define J2C_PACKED __attribute((packed))
 #endif
 
-#ifndef J2C_RESTRICT
-#define J2C_RESTRICT __restrict__
-#endif
-
 typedef enum J2C_PACKED {
-	CONSTANT_Class = 7,
-	CONSTANT_Fieldref = 9,
-	CONSTANT_Methodref = 10,
+	CONSTANT_Utf8				= 1,
+	CONSTANT_Integer			= 3,
+	CONSTANT_Float				= 4,
+	CONSTANT_Long				= 5,
+	CONSTANT_Double				= 6,
+	CONSTANT_Class				= 7,
+	CONSTANT_String				= 8,
+	CONSTANT_Fieldref			= 9,
+	CONSTANT_Methodref			= 10,
 	CONSTANT_InterfaceMethodref = 11,
-	CONSTANT_String = 8,
-	CONSTANT_Integer = 3,
-	CONSTANT_Float = 4,
-	CONSTANT_Long = 5,
-	CONSTANT_Double = 6,
-	CONSTANT_NameAndType = 12,
-	CONSTANT_Utf8 = 1,
-	CONSTANT_MethodHandle = 15,
-	CONSTANT_MethodType = 16,
-	CONSTANT_Dynamic = 17,
-	CONSTANT_InvokeDynamic = 18,
-	CONSTANT_Module = 19,
-	CONSTANT_Package = 20
+	CONSTANT_NameAndType		= 12,
+	CONSTANT_MethodHandle		= 15,
+	CONSTANT_MethodType			= 16,
+	CONSTANT_Dynamic			= 17,
+	CONSTANT_InvokeDynamic		= 18,
+	CONSTANT_Module				= 19,
+	CONSTANT_Package			= 20
 } cp_info_tag;
 
 typedef struct J2C_PACKED {
@@ -51,25 +49,25 @@ typedef struct J2C_PACKED {
 } cp_info;
 
 typedef enum J2C_PACKED {
-	ACC_PUBLIC = 0x0001,
-	ACC_PRIVATE = 0x0002,
-	ACC_PROTECTED = 0x0004,
-	ACC_STATIC = 0x0008,
-	ACC_FINAL = 0x0010,
-	ACC_SUPER = 0x0020,
+	ACC_PUBLIC		 = 0x0001,
+	ACC_PRIVATE		 = 0x0002,
+	ACC_PROTECTED	 = 0x0004,
+	ACC_STATIC		 = 0x0008,
+	ACC_FINAL		 = 0x0010,
+	ACC_SUPER		 = 0x0020,
 	ACC_SYNCHRONIZED = 0x0020,
-	ACC_VOLATILE = 0x0040,
-	ACC_BRIDGE = 0x0040,
-	ACC_TRANSIENT = 0x0080,
-	ACC_VARARGS = 0x0080,
-	ACC_NATIVE = 0x0100,
-	ACC_INTERFACE = 0x0200,
-	ACC_ABSTRACT = 0x0400,
-	ACC_STRICT = 0x0800,
-	ACC_SYNTHETIC = 0x1000,
-	ACC_ANNOTATION = 0x2000,
-	ACC_ENUM = 0x4000,
-	ACC_MODULE = 0x8000
+	ACC_VOLATILE	 = 0x0040,
+	ACC_BRIDGE		 = 0x0040,
+	ACC_TRANSIENT	 = 0x0080,
+	ACC_VARARGS		 = 0x0080,
+	ACC_NATIVE		 = 0x0100,
+	ACC_INTERFACE	 = 0x0200,
+	ACC_ABSTRACT	 = 0x0400,
+	ACC_STRICT		 = 0x0800,
+	ACC_SYNTHETIC	 = 0x1000,
+	ACC_ANNOTATION	 = 0x2000,
+	ACC_ENUM		 = 0x4000,
+	ACC_MODULE		 = 0x8000
 } access_flag;
 
 typedef struct J2C_PACKED {
@@ -133,8 +131,8 @@ static inline void *safemalloc(const size_t n) {
 /*
 Reads 1 unsigned byte from content and increments the idx.
 */
-static inline u1 read_u1(const u1 *J2C_RESTRICT content, const uint32_t length,
-						 uint32_t *J2C_RESTRICT idx) {
+static inline u1 read_u1(const u1 *content, const uint32_t length,
+						 uint32_t *idx) {
 	if (*idx >= length) {
 		error("\nError: called read_u1 with overflow\n");
 		exit(-1);
@@ -147,8 +145,8 @@ static inline u1 read_u1(const u1 *J2C_RESTRICT content, const uint32_t length,
 /*
 Reads 2 unsigned bytes from content in big-endian and increments the idx.
 */
-static inline u2 read_u2(const u1 *J2C_RESTRICT content, const uint32_t length,
-						 uint32_t *J2C_RESTRICT idx) {
+static inline u2 read_u2(const u1 *content, const uint32_t length,
+						 uint32_t *idx) {
 	if (*idx + 1 >= length) {
 		error("\nError: called read_u2 with overflow\n");
 		exit(-1);
@@ -161,8 +159,8 @@ static inline u2 read_u2(const u1 *J2C_RESTRICT content, const uint32_t length,
 /*
 Reads 4 unsigned bytes from content in big-endian and increments the idx.
 */
-static inline u4 read_u4(const u1 *J2C_RESTRICT content, const uint32_t length,
-						 uint32_t *J2C_RESTRICT idx) {
+static inline u4 read_u4(const u1 *content, const uint32_t length,
+						 uint32_t *idx) {
 	if (*idx + 3 >= length) {
 		fprintf(stderr, "\nError: called read_u4 with overflow\n");
 		exit(-1);
@@ -176,9 +174,8 @@ static inline u4 read_u4(const u1 *J2C_RESTRICT content, const uint32_t length,
 /*
 Reads N unsigned bytes from content in big-endian and increments the idx.
 */
-static inline void read_n(const u1 *J2C_RESTRICT content, const uint32_t length,
-						  uint32_t *J2C_RESTRICT idx, void *J2C_RESTRICT p,
-						  const u4 n) {
+static inline void read_n(const u1 *content, const uint32_t length,
+						  uint32_t *idx, void *p, const u4 n) {
 	if (n <= 0) {
 		// nothing to read
 		return;
@@ -192,34 +189,34 @@ static inline void read_n(const u1 *J2C_RESTRICT content, const uint32_t length,
 }
 
 // QUESTION: should this be returning void?
-static void read_cp_info(const u1 *J2C_RESTRICT content, const uint32_t length,
-						 uint32_t *J2C_RESTRICT idx,
-						 cp_info *J2C_RESTRICT cp_entry) {
-	const cp_info_tag tag = read_u1(content, length, idx);
-	u1 *info = NULL;
+static void read_cp_info(const u1 *content, const uint32_t length,
+						 uint32_t *idx, cp_info *cp_entry) {
+	const cp_info_tag tag = (cp_info_tag)read_u1(content, length, idx);
+	u1 *info			  = NULL;
+	u2 utf8_nbytes		  = 0;	// used only for CONSTANT_Utf8
 
 	// here no interpretation/check of the bit patterns is performed
 	switch (tag) {
 		case CONSTANT_Class:
-			info = safemalloc(sizeof(u1) * 2);
+			info = (u1 *)safemalloc(sizeof(u1) * 2);
 			read_n(content, length, idx, info, 2);
 			printf(" - CONSTANT_Class #%d\n",
 				   ((((u2)info[0]) << 8) | (u2)info[1]));
 			break;
 		case CONSTANT_String:
-			info = safemalloc(sizeof(u1) * 2);
+			info = (u1 *)safemalloc(sizeof(u1) * 2);
 			read_n(content, length, idx, info, 2);
 			printf(" - CONSTANT_String #%d\n",
 				   ((((u2)info[0]) << 8) | (u2)info[1]));
 			break;
 		case CONSTANT_MethodType:
-			info = safemalloc(sizeof(u1) * 2);
+			info = (u1 *)safemalloc(sizeof(u1) * 2);
 			read_n(content, length, idx, info, 2);
 			printf(" - CONSTANT_MethodType #%d\n",
 				   ((((u2)info[0]) << 8) | (u2)info[1]));
 			break;
 		case CONSTANT_Fieldref:
-			info = safemalloc(sizeof(u1) * 4);
+			info = (u1 *)safemalloc(sizeof(u1) * 4);
 			read_n(content, length, idx, info, 4);
 			printf(" - CONSTANT_Fieldref\n");
 			printf(" - class         #%d\n",
@@ -228,7 +225,7 @@ static void read_cp_info(const u1 *J2C_RESTRICT content, const uint32_t length,
 				   ((((u2)info[2]) << 8) | (u2)info[3]));
 			break;
 		case CONSTANT_Methodref:
-			info = safemalloc(sizeof(u1) * 4);
+			info = (u1 *)safemalloc(sizeof(u1) * 4);
 			read_n(content, length, idx, info, 4);
 			printf(" - CONSTANT_Methodref\n");
 			printf(" - class         #%d\n",
@@ -237,7 +234,7 @@ static void read_cp_info(const u1 *J2C_RESTRICT content, const uint32_t length,
 				   ((((u2)info[2]) << 8) | (u2)info[3]));
 			break;
 		case CONSTANT_InterfaceMethodref:
-			info = safemalloc(sizeof(u1) * 4);
+			info = (u1 *)safemalloc(sizeof(u1) * 4);
 			read_n(content, length, idx, info, 4);
 			printf(" - CONSTANT_InterfaceMethodref\n");
 			printf(" - class         #%d\n",
@@ -247,7 +244,7 @@ static void read_cp_info(const u1 *J2C_RESTRICT content, const uint32_t length,
 			break;
 		case CONSTANT_Integer:
 			printf(" - CONSTANT_Integer\n");
-			info = safemalloc(sizeof(u1) * 4);
+			info = (u1 *)safemalloc(sizeof(u1) * 4);
 			read_n(content, length, idx, info, 4);
 			{
 				const u4 x = (((u4)info[0]) << 24) | (((u4)info[1]) << 16) |
@@ -257,7 +254,7 @@ static void read_cp_info(const u1 *J2C_RESTRICT content, const uint32_t length,
 			break;
 		case CONSTANT_Float:
 			printf(" - CONSTANT_Float\n");
-			info = safemalloc(sizeof(u1) * 4);
+			info = (u1 *)safemalloc(sizeof(u1) * 4);
 			read_n(content, length, idx, info, 4);
 			{
 				const u4 x = (((u4)info[0]) << 24) | (((u4)info[1]) << 16) |
@@ -267,14 +264,14 @@ static void read_cp_info(const u1 *J2C_RESTRICT content, const uint32_t length,
 			break;
 		case CONSTANT_NameAndType:
 			printf(" - CONSTANT_NameAndType\n");
-			info = safemalloc(sizeof(u1) * 4);
+			info = (u1 *)safemalloc(sizeof(u1) * 4);
 			read_n(content, length, idx, info, 4);
 			printf(" - name       #%d\n", ((((u2)info[0]) << 8) | (u2)info[1]));
 			printf(" - descriptor #%d\n", ((((u2)info[2]) << 8) | (u2)info[3]));
 			break;
 		case CONSTANT_InvokeDynamic:
 			printf(" - CONSTANT_InvokeDynamic\n");
-			info = safemalloc(sizeof(u1) * 4);
+			info = (u1 *)safemalloc(sizeof(u1) * 4);
 			read_n(content, length, idx, info, 4);
 			printf(" - bootstrap_method_attr_index #%d\n",
 				   ((((u2)info[0]) << 8) | (u2)info[1]));
@@ -283,7 +280,7 @@ static void read_cp_info(const u1 *J2C_RESTRICT content, const uint32_t length,
 			break;
 		case CONSTANT_Long:
 			printf(" - CONSTANT_Long\n");
-			info = safemalloc(sizeof(u1) * 8);
+			info = (u1 *)safemalloc(sizeof(u1) * 8);
 			read_n(content, length, idx, info, 8);
 			{
 				const u8 x = (((u8)info[0]) << 56) | (((u8)info[1]) << 48) |
@@ -295,7 +292,7 @@ static void read_cp_info(const u1 *J2C_RESTRICT content, const uint32_t length,
 			break;
 		case CONSTANT_Double:
 			printf(" - CONSTANT_Double\n");
-			info = safemalloc(sizeof(u1) * 8);
+			info = (u1 *)safemalloc(sizeof(u1) * 8);
 			read_n(content, length, idx, info, 8);
 			{
 				const u8 x = (((u8)info[0]) << 56) | (((u8)info[1]) << 48) |
@@ -306,9 +303,9 @@ static void read_cp_info(const u1 *J2C_RESTRICT content, const uint32_t length,
 			}
 			break;
 		case CONSTANT_Utf8:
-			const u2 utf8_nbytes = read_u2(content, length, idx);
+			utf8_nbytes = read_u2(content, length, idx);
 			printf(" - CONSTANT_Utf8\n");
-			info = safemalloc(sizeof(u2) + sizeof(u1) * utf8_nbytes);
+			info = (u1 *)safemalloc(sizeof(u2) + sizeof(u1) * utf8_nbytes);
 			read_n(content, length, idx, info,
 				   sizeof(u2) + sizeof(u1) * utf8_nbytes);
 			printf(" - \"");
@@ -336,42 +333,39 @@ static void read_cp_info(const u1 *J2C_RESTRICT content, const uint32_t length,
 			break;
 		case CONSTANT_MethodHandle:
 			printf(" - CONSTANT_MethodHandle\n");
-			info = safemalloc(sizeof(u1) * 3);
+			info = (u1 *)safemalloc(sizeof(u1) * 3);
 			read_n(content, length, idx, info, 3);
 			break;
 		default:
 			fprintf(stderr, "\nError: Unknown cp_info tag %d (0x%02X)\n", tag,
 					tag);
-			// *idx += 2;
 			exit(-1);
 			return;
 	}
 
-	cp_entry->tag = tag;
+	cp_entry->tag  = tag;
 	cp_entry->info = info;
 }
 
 // QUESTION: should this be returning void?
-static void read_attribute_info(const u1 *J2C_RESTRICT content,
-								const uint32_t length,
-								uint32_t *J2C_RESTRICT idx,
-								attribute_info *J2C_RESTRICT attribute) {
+static void read_attribute_info(const u1 *content, const uint32_t length,
+								uint32_t *idx, attribute_info *attribute) {
 	attribute->attribute_name_index = read_u2(content, length, idx);
-	attribute->attribute_length = read_u4(content, length, idx);
-	attribute->info = safemalloc(attribute->attribute_length * sizeof(u1));
+	attribute->attribute_length		= read_u4(content, length, idx);
+	attribute->info =
+		(u1 *)safemalloc(attribute->attribute_length * sizeof(u1));
 	read_n(content, length, idx, attribute->info, attribute->attribute_length);
 }
 
 // QUESTION: should this be returning void?
-static void read_field_info(const u1 *J2C_RESTRICT content,
-							const uint32_t length, uint32_t *J2C_RESTRICT idx,
-							field_info *J2C_RESTRICT field) {
-	field->access_flags = read_u2(content, length, idx);
-	field->name_index = read_u2(content, length, idx);
+static void read_field_info(const u1 *content, const uint32_t length,
+							uint32_t *idx, field_info *field) {
+	field->access_flags		= (access_flag)read_u2(content, length, idx);
+	field->name_index		= read_u2(content, length, idx);
 	field->descriptor_index = read_u2(content, length, idx);
 	field->attributes_count = read_u2(content, length, idx);
-	field->attributes =
-		safemalloc(field->attributes_count * sizeof(attribute_info));
+	field->attributes = (attribute_info *)safemalloc(field->attributes_count *
+													 sizeof(attribute_info));
 
 	for (u2 i = 0; i < field->attributes_count; i++) {
 		read_attribute_info(content, length, idx, &field->attributes[i]);
@@ -379,15 +373,14 @@ static void read_field_info(const u1 *J2C_RESTRICT content,
 }
 
 // QUESTION: should this be returning void?
-static void read_method_info(const u1 *J2C_RESTRICT content,
-							 const uint32_t length, uint32_t *J2C_RESTRICT idx,
-							 method_info *J2C_RESTRICT method) {
-	method->access_flags = read_u2(content, length, idx);
-	method->name_index = read_u2(content, length, idx);
+static void read_method_info(const u1 *content, const uint32_t length,
+							 uint32_t *idx, method_info *method) {
+	method->access_flags	 = (access_flag)read_u2(content, length, idx);
+	method->name_index		 = read_u2(content, length, idx);
 	method->descriptor_index = read_u2(content, length, idx);
 	method->attributes_count = read_u2(content, length, idx);
-	method->attributes =
-		safemalloc(method->attributes_count * sizeof(attribute_info));
+	method->attributes = (attribute_info *)safemalloc(method->attributes_count *
+													  sizeof(attribute_info));
 
 	for (u2 i = 0; i < method->attributes_count; i++) {
 		read_attribute_info(content, length, idx, &method->attributes[i]);
@@ -398,46 +391,48 @@ static void read_method_info(const u1 *J2C_RESTRICT content,
 Parses the 'content' array (containing the contents of a class file)
 and returns an allocated and initialized class_file struct.
 */
-static class_file *parse_class_file(const u1 *J2C_RESTRICT content,
-									const uint32_t length) {
+static class_file *parse_class_file(const u1 *content, const uint32_t length) {
 	if (content == NULL || length <= 0) {
 		return NULL;
 	}
 
 	uint32_t idx = 0;
 
-	const u4 magic_number = read_u4(content, length, &idx);
-	const u2 minor_version = read_u2(content, length, &idx);
-	const u2 major_version = read_u2(content, length, &idx);
+	const u4 magic_number		 = read_u4(content, length, &idx);
+	const u2 minor_version		 = read_u2(content, length, &idx);
+	const u2 major_version		 = read_u2(content, length, &idx);
 	const u2 constant_pool_count = read_u2(content, length, &idx);
 
 	cp_info *constant_pool =
 		(cp_info *)safemalloc((constant_pool_count - 1) * sizeof(cp_info));
 
+	printf("Constant pool\n");
 	for (u2 i = 0; i < constant_pool_count - 1; i++) {
-		printf("Constant pool entry #%d\n", i + 1);
+		printf("#%5d\n", i + 1);
 		read_cp_info(content, length, &idx, &constant_pool[i]);
 	}
 
-	const u2 access_flags = read_u2(content, length, &idx);
-	const u2 this_class = read_u2(content, length, &idx);
-	const u2 super_class = read_u2(content, length, &idx);
+	const u2 access_flags	  = read_u2(content, length, &idx);
+	const u2 this_class		  = read_u2(content, length, &idx);
+	const u2 super_class	  = read_u2(content, length, &idx);
 	const u2 interfaces_count = read_u2(content, length, &idx);
-	u2 *interfaces = safemalloc(interfaces_count * sizeof(u2));
+	u2 *interfaces			  = (u2 *)safemalloc(interfaces_count * sizeof(u2));
 
 	for (u2 i = 0; i < interfaces_count; i++) {
 		interfaces[i] = read_u2(content, length, &idx);
 	}
 
 	const u2 fields_count = read_u2(content, length, &idx);
-	field_info *fields = safemalloc(fields_count * sizeof(field_info));
+	field_info *fields =
+		(field_info *)safemalloc(fields_count * sizeof(field_info));
 
 	for (u2 i = 0; i < fields_count; i++) {
 		read_field_info(content, length, &idx, &fields[i]);
 	}
 
 	const u2 methods_count = read_u2(content, length, &idx);
-	method_info *methods = safemalloc(methods_count * sizeof(method_info));
+	method_info *methods =
+		(method_info *)safemalloc(methods_count * sizeof(method_info));
 
 	for (u2 i = 0; i < methods_count; i++) {
 		read_method_info(content, length, &idx, &methods[i]);
@@ -445,7 +440,7 @@ static class_file *parse_class_file(const u1 *J2C_RESTRICT content,
 
 	const u2 attributes_count = read_u2(content, length, &idx);
 	attribute_info *attributes =
-		safemalloc(attributes_count * sizeof(attribute_info));
+		(attribute_info *)safemalloc(attributes_count * sizeof(attribute_info));
 
 	for (u2 i = 0; i < attributes_count; i++) {
 		read_attribute_info(content, length, &idx, &attributes[i]);
@@ -461,24 +456,24 @@ static class_file *parse_class_file(const u1 *J2C_RESTRICT content,
 	printf("%d methods\n", methods_count);
 	printf("%d attributes\n", attributes_count);
 
-	class_file *cf = safemalloc(sizeof(class_file));
+	class_file *cf = (class_file *)safemalloc(sizeof(class_file));
 
-	cf->magic_number = magic_number;
-	cf->minor_version;
-	cf->major_version = major_version;
+	cf->magic_number		= magic_number;
+	cf->minor_version		= minor_version;
+	cf->major_version		= major_version;
 	cf->constant_pool_count = constant_pool_count;
-	cf->constant_pool = constant_pool;
-	cf->access_flags = access_flags;
-	cf->this_class = this_class;
-	cf->super_class = super_class;
-	cf->interfaces_count = interfaces_count;
-	cf->interfaces = interfaces;
-	cf->fields_count = fields_count;
-	cf->fields = fields;
-	cf->methods_count = methods_count;
-	cf->methods = methods;
-	cf->attributes_count = attributes_count;
-	cf->attributes = attributes;
+	cf->constant_pool		= constant_pool;
+	cf->access_flags		= (access_flag)access_flags;
+	cf->this_class			= this_class;
+	cf->super_class			= super_class;
+	cf->interfaces_count	= interfaces_count;
+	cf->interfaces			= interfaces;
+	cf->fields_count		= fields_count;
+	cf->fields				= fields;
+	cf->methods_count		= methods_count;
+	cf->methods				= methods;
+	cf->attributes_count	= attributes_count;
+	cf->attributes			= attributes;
 
 	return cf;
 }
@@ -489,7 +484,7 @@ Reads the .class file fully, if successfull, and returns a
 pointer to a class_file structure allocated dinamically.
 This class_file needs to be deallocated with class_file_destroy().
 */
-class_file *read_class_file(const char *J2C_RESTRICT filename) {
+class_file *read_class_file(const char *filename) {
 	if (filename == NULL) {
 		fprintf(stderr, "\nError: Could not open NULL filename\n");
 		return NULL;
@@ -544,7 +539,7 @@ class_file *read_class_file(const char *J2C_RESTRICT filename) {
 /*
 Deallocates the memory used by the given class_file structure.
 */
-void class_file_destroy(class_file *J2C_RESTRICT cf) {
+void class_file_destroy(class_file *cf) {
 	for (u2 i = 0; i < cf->constant_pool_count - 1; i++) {
 		free(cf->constant_pool[i].info);
 	}
